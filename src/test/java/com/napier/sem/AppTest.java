847d9eec1f14cc9a4f5db9cbe596e0f5eaa73e10
Rory <40492068@live.napier.ac.uk>
2022-03-10 15:38:19 +0000

    All HR system test working

--- src/test/java/com/napier/sem/AppTest.java
+++ src/test/java/com/napier/sem/AppTest.java
@@ -23,4 +23,30 @@ public class AppTest
     {
         app.printSalaries(null);
     }
+    @Test
+    void printSalariesTestEmpty()
+    {
+        ArrayList<Employee> employess = new ArrayList<Employee>();
+        app.printSalaries(employess);
+    }
+    @Test
+    void printSalariesTestContainsNull()
+    {
+        ArrayList<Employee> employess = new ArrayList<Employee>();
+        employess.add(null);
+        app.printSalaries(employess);
+    }
+    @Test
+    void printSalaries()
+    {
+        ArrayList<Employee> employees = new ArrayList<Employee>();
+        Employee emp = new Employee();
+        emp.emp_no = 1;
+        emp.first_name = "Kevin";
+        emp.last_name = "Chalmers";
+        emp.title = "Engineer";
+        emp.salary = 55000;
+        employees.add(emp);
+        app.printSalaries(employees);
+    }
 }
